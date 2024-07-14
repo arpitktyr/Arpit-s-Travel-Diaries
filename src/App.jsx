8016277bcebeb70334639c770ab0224ebed54eb3
arpitktyr <arpitktyr@gmail.com>
2024-07-14 07:22:45 +0530

    First Comit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <div class="main-content">
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Page 1</h1>
+      {/* Your page 1 content here */}
+    </div>
+  );
+};
+
+export default Home;
